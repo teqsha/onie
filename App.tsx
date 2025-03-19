c1acd53ac6c8a233483294aacef8ccfcf2952d42
nikita munda <nikitamunda42@gmail.com>
2025-03-19 13:57:01 +0530

    app file update

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Hook() {
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Hook</Text> 
-    </SafeAreaView>
-  );
-};
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+export default App
+
+const styles = StyleSheet.create({})
