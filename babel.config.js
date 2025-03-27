6aed8236ff6603cd1cad8a9c565bd61eef059baf
teqsha <teqshaofficial@gmail.com>
2025-03-27 12:22:27 +0530

    commit initial

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-<<<<<<< HEAD
-=======
-  // plugins: ['react-native-reanimated/plugin'],
->>>>>>> be2844b5aaaa7b6df4f0e65374fb47c854e41279
 };
