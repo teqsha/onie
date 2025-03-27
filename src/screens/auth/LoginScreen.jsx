b56ec050bedb4138fb93721e7142077280a4d264
teqsha <teqshaofficial@gmail.com>
2025-03-27 11:55:42 +0530

    Login file update

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -17,7 +17,7 @@ const SocialButton = ({ text, iconSource, onPress }) => {
 };
 const LoginScreen = () => {
 const navigation = useNavigation();
-  // const dispatch = useDispatch();;;
+  // const dispatch = useDispatch();;x;
   const [hidePassword, setHidePassword] = useState(true);
 
   const handleLogin = (values) => {
